28b5b27058ba46f107840b9f699c8403da861b86
Nasller <nasller@qq.com>
2024-05-27 14:56:30 +0800

    feat(8.15): 1

--- x-pack/plugin/core/src/main/java/org/elasticsearch/license/LicenseVerifier.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/license/LicenseVerifier.java
@@ -8,30 +8,13 @@ package org.elasticsearch.license;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.lucene.util.BytesRef;
-import org.apache.lucene.util.BytesRefIterator;
-import org.elasticsearch.common.bytes.BytesReference;
 import org.elasticsearch.common.hash.MessageDigests;
 import org.elasticsearch.core.Streams;
-import org.elasticsearch.xcontent.ToXContent;
-import org.elasticsearch.xcontent.XContentBuilder;
-import org.elasticsearch.xcontent.XContentFactory;
-import org.elasticsearch.xcontent.XContentType;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.ByteBuffer;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
-import java.security.Signature;
-import java.security.SignatureException;
-import java.util.Arrays;
-import java.util.Base64;
-import java.util.Collections;
-
-import static org.elasticsearch.core.Strings.format;
 
 /**
  * Responsible for verifying signed licenses
@@ -47,49 +30,7 @@ public class LicenseVerifier {
      * @return true if valid, false otherwise
      */
     public static boolean verifyLicense(final License license, PublicKey publicKey) {
-        byte[] signedContent = null;
-        byte[] publicKeyFingerprint = null;
-        try {
-            byte[] signatureBytes = Base64.getDecoder().decode(license.signature());
-            ByteBuffer byteBuffer = ByteBuffer.wrap(signatureBytes);
-            @SuppressWarnings("unused")
-            int version = byteBuffer.getInt();
-            int magicLen = byteBuffer.getInt();
-            byte[] magic = new byte[magicLen];
-            byteBuffer.get(magic);
-            int hashLen = byteBuffer.getInt();
-            publicKeyFingerprint = new byte[hashLen];
-            byteBuffer.get(publicKeyFingerprint);
-            int signedContentLen = byteBuffer.getInt();
-            signedContent = new byte[signedContentLen];
-            byteBuffer.get(signedContent);
-            XContentBuilder contentBuilder = XContentFactory.contentBuilder(XContentType.JSON);
-            license.toXContent(contentBuilder, new ToXContent.MapParams(Collections.singletonMap(License.LICENSE_SPEC_VIEW_MODE, "true")));
-            Signature rsa = Signature.getInstance("SHA512withRSA");
-            rsa.initVerify(publicKey);
-            BytesRefIterator iterator = BytesReference.bytes(contentBuilder).iterator();
-            BytesRef ref;
-            while ((ref = iterator.next()) != null) {
-                rsa.update(ref.bytes, ref.offset, ref.length);
-            }
-            boolean verifyResult = rsa.verify(signedContent);
-            if (verifyResult == false) {
-                logger.warn(
-                    () -> format(
-                        "License with uid [%s] failed signature verification with the public key with sha256 [%s].",
-                        license.uid(),
-                        PUBLIC_KEY_DIGEST_HEX_STRING
-                    )
-                );
-            }
-            return verifyResult;
-        } catch (IOException | NoSuchAlgorithmException | SignatureException | InvalidKeyException e) {
-            throw new IllegalStateException(e);
-        } finally {
-            if (signedContent != null) {
-                Arrays.fill(signedContent, (byte) 0);
-            }
-        }
+        return true;
     }
 
     private static final PublicKey PUBLIC_KEY;
